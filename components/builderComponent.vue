ec2637c1a87c94b8e3a2a87bddb2f121d273b04f
Surinder Singh <surinder-s001@webreinvent.com>
2024-02-05 12:19:13 +0530

    implented targeting with btn to change state manually and added default targeting.

--- components/builderComponent.vue
+++ components/builderComponent.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="home">
     <div v-if="content || isPreviewing()">
+      <button class="age-toggle-btn" @click="toggleAgeGroup()">Click to toggle Age Group</button>
       <RenderContent
           model="surinder-2"
           :content="content"
           :api-key="BUILDER_PUBLIC_API_KEY"
-          :data='{agegroup: "child"}'
+          :data='{agegroup: agegroup}'
       />
     </div>
-    <div v-else>Content not Found</div>
+    <div v-else>Loading Content...</div>
   </div>
 </template>
 
@@ -19,18 +20,44 @@ import {
   isPreviewing,
 } from "@builder.io/sdk-vue";
 
+const agegroup = ref("default");
+const content = ref(null);
 
 const BUILDER_PUBLIC_API_KEY = "fdc38f2e8eee4fcc90e72279fdb8708c";
 const route = useRoute();
 
+watchEffect(async () => {
 // fetch builder content data
-const content = await fetchOneEntry({
-  model: "surinder-2",
-  apiKey: BUILDER_PUBLIC_API_KEY,
-  userAttributes: {
-    urlPath: route.path,
-    agegroup: "child",
-  },
+  const data = await fetchOneEntry({
+    model: "surinder-2",
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    userAttributes: {
+      urlPath: route.path,
+      agegroup: agegroup.value,
+    },
+  });
+  content.value = data;
 });
 
+const toggleAgeGroup = () => {
+  agegroup.value = agegroup.value === "child" ? "adult" : "child";
+}
+
 </script>
+
+<style scoped>
+.age-toggle-btn {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 9999;
+}
+</style>
